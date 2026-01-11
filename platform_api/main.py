aa39de1b160db9972aadb8ed65a0808434b57a9d
Rohit Mohan <rohit.mohan@meetpoppins.com>
2026-01-11 09:59:32 -0500

    functions to shorten a url and redirect to original

--- platform_api/main.py
+++ platform_api/main.py
@@ -1,8 +1,59 @@
-from flask import Flask
+import string
+import time
 
+from flask import Flask
+from flask import request
+from flask import redirect
+
+
+links = {}
 
 app = Flask(__name__)
 
-@app.route("/")
+
+def generate_shortcode():
+    # use base62 to generate a shortcode using the current timestamp
+    number = int(time.time())
+
+    alphabet = string.ascii_lowercase + string.ascii_uppercase + string.digits
+
+    shortcode = ""
+
+    while number > 0:
+        remainder = number % 62
+
+        shortcode = alphabet[remainder] + shortcode
+
+        number //= 62
+
+
+    return shortcode
+
+
+@app.get("/")
 def index():
     return { "message": "Hello, Urly!" }
+
+
+@app.post("/shorten")
+def shorten_url():
+    payload = request.json
+
+    if "url" not in payload:
+        return { "error": "Please provide the URL to shorten "}, 422
+
+    shortcode = generate_shortcode()
+
+    links[shortcode] = payload["url"]
+
+    return { "url": f"http://localhost:5000/{shortcode}" }
+
+
+@app.get("/<shortcode>")
+def redirect_to_url(shortcode):
+    link = links.get(shortcode) or ""
+
+    if not link:
+        return {"error": f"There's no URL for {shortcode}"}, 404
+
+    return redirect(links[shortcode], code=301)
